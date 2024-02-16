25b6c431e56ba354e64e2be9c5779c71c50e2cbc
dmolitor <molitdj97@gmail.com>
2024-02-16 10:17:13 -0500

    Initial commit

---
+++ example.py
@@ -0,0 +1,53 @@
+import json
+import pandas as pd
+import xgboost as xgb
+from xgb_gridsearch.grid_search import GridSearch
+
+# Test with data ----------------------------------------------------------
+
+# Import data
+parents = pd.read_csv("https://raw.githubusercontent.com/info3370/info3370.github.io/main/data/parents.csv")
+parents["sex"] = parents.sex.astype("category")
+
+# Make a testing set
+parents_test = pd.DataFrame([{"sex": "female", "child_age": 1}])
+parents_test["sex"] = parents_test.sex.astype("category")
+
+# Get into DMatrix form
+data_train = xgb.DMatrix(
+    data = parents.drop("at_work", axis=1),
+    label=parents[["at_work"]],
+    enable_categorical=True
+)
+data_test = xgb.DMatrix(data = parents_test, enable_categorical=True)
+
+# Set parameters to grid search across
+params = {
+    "objective": ["binary:logistic"],
+    "learning_rate": [0.1, 0.3],
+    "max_depth": [4, 6, 8],
+    "grow_policy": ["depthwise"],
+    "min_child_weight": [0.5, 1, 3],
+    "max_leaves": [0, 3, 5, 7]
+}
+
+# Create grid search object
+grid_search = GridSearch(
+    param_grid=params,
+    dtrain=data_train,
+    num_boost_round=1000,
+    nfold=5,
+    shuffle=True,
+    metrics=["auc", "logloss"],
+    early_stopping_rounds=5,
+    verbose_eval=False
+)
+grid_search.fit(verbose=True, minimize_cv_metric=True, refit=True)
+
+# Get predicted proportion of working mothers with 1-year-olds
+prop_working = round(grid_search.best_model.predict(data_test)[0], 4)
+
+# Print optimal parameters
+print(json.dumps(grid_search.best_parameters, indent=4))
+print("Predicted proportion working mothers:", prop_working)
+
---
+++ xgb_gridsearch/grid_search.py
@@ -0,0 +1,171 @@
+from itertools import product
+import pandas as pd
+from tqdm import tqdm
+from typing import Any
+import xgboost as xgb
+
+## Exceptions ##
+
+class ParameterFormatError(Exception):
+    pass
+
+class ArgumentValueError(Exception):
+    pass
+
+## Grid Search class ##
+
+class GridSearch:
+    """A simple class to perform parameter grid search using xgboost.cv"""
+    def __init__(self, param_grid: dict, **kwargs):
+        """Key word arguments are passed directly in to xgboost.cv"""
+        validate_kwargs(**kwargs)
+        self._booster_args = kwargs
+        if "metrics" in kwargs:
+            metrics = kwargs.get("metrics")
+            if isinstance(metrics, list):
+                target_metric = metrics[len(metrics) - 1]
+            else:
+                target_metric = metrics
+            self._target_metric = target_metric
+        self.best_idx = None
+        self.best_model = None
+        self.best_parameters = None
+        self.param_grid = expand_grid(param_grid)
+    
+    def _best_parameters(self, min: bool = True):
+        """
+        Find the optimal model paramters based on minimized (or maximized)
+        cross-validation evaluation metrics
+        """
+        target_metric = self._target_metric
+        if target_metric is None:
+            target_metrics = self.model_metrics.iloc[:, 2]
+        else:
+            target_metrics = self.model_metrics[f"test-{target_metric}-mean"]
+        if min:
+            optimal_idx = target_metrics.idxmin()
+        else:
+            optimal_idx = target_metrics.idxmax()
+        optimal_params = (
+            self
+            .param_grid
+            .iloc[optimal_idx]
+            .to_dict()
+        )
+        optimal_num_boost = (
+            self
+            .model_metrics
+            .iloc[optimal_idx]["num_boost_round"]
+        )
+        self.best_idx = optimal_idx
+        self.best_parameters = optimal_params
+        self.best_num_boost_round = int(optimal_num_boost)
+    
+    def _drop_cv_args(self, **kwargs):
+        train_args = [
+            "params",
+            "dtrain",
+            "num_boost_round",
+            "evals",
+            "obj",
+            "feval",
+            "maximize",
+            "evals_result",
+            "verbose_eval",
+            "xgb_model",
+            "callbacks",
+            "custom_metric"
+        ]
+        for k in list(kwargs.keys()).copy():
+            if k not in train_args:
+                del kwargs[k]
+        return kwargs
+    
+    def _fit_with_params(self, params: dict) -> pd.DataFrame:
+        """Fit a boosted model with a single set of parameter values"""
+        model = xgb.cv(params=params, **self._booster_args)
+        model_error = model.iloc[(len(model) - 1):len(model)]
+        model_error = model_error.assign(num_boost_round = len(model))
+        return model_error
+    
+    def _fit_param_grid(self, verbose: bool = True) -> pd.DataFrame:
+        """Fit a boosted model across every row in our parameter grid"""
+        param_grid = self.param_grid
+        model_error = []
+        if verbose:
+            param_iter = tqdm(param_grid.iterrows(), total=len(param_grid))
+        else:
+            param_iter = param_grid.iterrows()
+        for params in param_iter:
+            error = self._fit_with_params(params=params[1].to_dict())
+            model_error.append(error)
+        model_error_df = pd.concat(model_error, axis=0).reset_index(drop=True)
+        self.model_metrics = model_error_df
+    
+    def fit(self,
+            verbose: bool = True,
+            minimize_cv_metric: bool = True,
+            refit: bool = True):
+        """
+        Fit a boosted tree model across every parameter in our tuning grid.
+        Then, find the optimal model parameters and, if desired, fit a final
+        boosted tree model using these optimal parameters.
+
+        Parameters
+        ----------
+        verbose:
+            A boolean. Display model training progress across tuning grid.
+        minimize_cv_metric:
+            A boolean. True indicates the optimal parameters are those that
+            minimize the cross-validation performance metric. Otherwise the
+            optimal parameters are those that maximize the performance metric.
+        refit:
+            A boolean. Refit a boosted tree using the optimal parameters on
+            the whole dataset.
+        """
+        self._fit_param_grid(verbose=verbose)
+        self._best_parameters(min=minimize_cv_metric)
+        if refit:
+            optimal_params = self.best_parameters
+            booster_args = self._drop_cv_args(**self._booster_args)
+            booster_args["num_boost_round"] = self.best_num_boost_round
+            best_model = xgb.train(params=optimal_params, **booster_args)
+            self.best_model = best_model
+
+## Utility functions ##
+
+def expand_grid(params: dict) -> pd.DataFrame:
+    """Replicates R's expand.grid function"""
+    validate_params(params)
+    cols = list(params.keys())
+    grid = list(product(*params.values()))
+    grid = pd.DataFrame(grid, columns=cols)
+    return grid
+
+def is_nonempty_list(x: list | Any) -> bool:
+    is_list = isinstance(x, list)
+    empty = not x
+    valid = is_list and not empty
+    return valid
+
+def validate_kwargs(**kwargs) -> bool:
+    """Throws an error if any kwargs are None or an empty list"""
+    for k, v in kwargs.items():
+        valid = v is not None
+        if valid and isinstance(v, list):
+            valid = is_nonempty_list(v)
+        if not valid:
+            raise ArgumentValueError(
+                f"Argument `{k}` must not be None if provided"
+            )
+    return True
+
+def validate_params(params: dict) -> bool:
+    """Throws an error if any parameters are unboxed (aka not a list)"""
+    for k, v in params.items():
+        valid = is_nonempty_list(v)
+        if not valid:
+            raise ParameterFormatError(
+                f"Parameter `{k}` must be a non-empty list"
+            )
+    return True
